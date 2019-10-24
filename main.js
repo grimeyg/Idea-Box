b89fe20a14bdc1a25cf626b3a9dd762ab8ebb6b3
Carla Geglio <carla.geglio@gmail.com>
2019-10-23 19:16:29 -0600

    Disable save button until input fields filled

---
+++ main.js
@@ -0,0 +1,16 @@
+var saveBtn = document.querySelector(".save");
+var titleIn = document.querySelector(".title");
+var bodyIn = document.querySelector("textarea");
+var form = document.querySelector("form");
+
+saveBtn.disabled = true;
+form.addEventListener("keyup", checkInputs);
+
+function checkInputs() {
+  event.preventDefault();
+   if (bodyIn.value.length > 0 && titleIn.value.length > 0) {
+    saveBtn.disabled = false;
+    saveBtn.style.cursor = "pointer";
+    saveBtn.style.background = "#343567";
+  }
+}

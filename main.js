875f1ca82547bd0eef28962766ad919cb8492b38
Carla Geglio <carla.geglio@gmail.com>
2019-10-30 14:33:37 -0600

    Remove commented out text

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ var form = document.querySelector("form");
 var saveBtn = document.querySelector(".save");
 var userInput = document.querySelector(".user-input");
 var cards = [];
-// var favorite = false;
 var pageOpacity = document.querySelector(".page-opacity")
 var filterMobile = document.querySelector(".mobile-filter");
 var menuClosed = document.querySelector(".menu-icon");
@@ -13,15 +12,16 @@ var sidebar = document.querySelector(".sidebar");
 saveBtn.disabled = true;
 var menuOpen = false;
 var starArr =[];
-// var Idea = require('../ideas')
-// window.addEventListener("load", pullCard);
+
 window.addEventListener("load", function(){
   if (localStorage !== null) {
     pullCard();
   }
 });
+
 userInput.addEventListener("keyup", checkInputs);
 menuClosed.addEventListener("click", dropMenu);
+
 cardContainer.addEventListener("click", function(event) {
   event.preventDefault();
   if (event.target.className === "star") {
@@ -29,17 +29,15 @@ cardContainer.addEventListener("click", function(event) {
   }
   if (event.target.className === "delete") {
     removeCard();
-    // event.target.deleteFromStorage;
   }
 });
+
 form.addEventListener("click", function(event) {
   if (event.target.className === "save") {
     inputFromForm();
-    // addCard();
-    // for (var i = 0; i < cards.length; i++) {
-      // cards[i].saveToStorage();
     }
 });
+
 function dropMenu () {
   menuOpen = !menuOpen;
   if (menuOpen) {
@@ -54,6 +52,7 @@ function dropMenu () {
     pageOpacity.style.display = "none";
   }
 };
+
 function checkInputs(event) {
   event.preventDefault();
    if (userInput.value) {
@@ -61,31 +60,20 @@ function checkInputs(event) {
     saveBtn.id = "active";
   }
 };
-// 1. Find the id of the idea that we clicked on
-// 2. Use that id to find the specific idea in your cards array
-// 3. Once you have that idea, you can update the cards array to
-// include everything BUT that card
-// 4. call you saveToStorage() using the idea you currently have selected
-// 5. Clear out the DOM and reload all the cards
+
 function removeCard() {
   var targetId = parseInt(event.target.id, 10);
   var deleted = event.target.closest("card");
   deleted = new Idea;
   for (var i = 0; i < cards.length; i++) {
-    // console.log(targetId);
-    // console.log(cards);
     if (targetId === cards[i].id) {
-      // cardContainer.remove(2);
       cards.splice(i, 1);
-      console.log(cards);
-      // deleted.deleteFromStorage(deleted);
       deleted.saveToStorage(cards);
     }
   }
 }
+
 function addCard(card) {
-  // for (var i = 0; i < cards.length; i++) {
-  // var recent = cards[cards.length - 1];
   cardContainer.innerHTML += `
   <div class="card">
     <header>
@@ -105,8 +93,6 @@ function addCard(card) {
 
 
 function addFavoriteCard(card) {
-  // for (var i = 0; i < cards.length; i++) {
-  // var recent = cards[cards.length - 1];
   cardContainer.innerHTML += `
   <div class="card">
     <header>
@@ -123,45 +109,25 @@ function addFavoriteCard(card) {
   saveBtn.disabled = true;
   saveBtn.id = "";
 }
-// }
-// }
+
 function inputFromForm() {
   var id = Date.now();
   var newIdea = new Idea(titleInput.value, bodyInput.value, id);
   cards.push(newIdea);
   newIdea.saveToStorage(cards);
   addCard(newIdea);
-  // for (var i = 0; i < cards.length; i++){
-  // localStorage.setItem("cardContainer", JSON.stringify(cards));
   form.reset();
 };
 
 function inputFromStorage(card) {
   card = new Idea(card.title, card.body, card.id, card.favorite);
-
-  //   var matchingStar = cardContainer.querySelectorAll(".star");
-  //   console.log(matchingStar);
-  //   //
-  //   starArr.push(matchingStar);
-  //   console.log(starArr);
-  //   for (var i = 0; i < starArr.length; i++) {
-  //     if (starArr[i] === card.id) {
-  //       starrArr[i].setAttribute("src","images/star-active.svg");
-  //     }
-  //   }
-
-
-  //   event.target.setAttribute("src","images/star-active.svg");
-  // } else {
-  //   event.target.setAttribute("src","images/star.svg");
-    // }
   cards.push(card);
   card.saveToStorage(cards);
   if (card.favorite) {
     addFavoriteCard(card)
   } else {
   addCard(card);
-}
+  }
 }
 
 
@@ -170,51 +136,16 @@ function pullCard() {
   var cardArray = JSON.parse(arrayOfObjects);
   for (i = 0; i < cardArray.length; i++) {
     console.log(cardArray[i]);
-    // cardArray[i] = new Idea(cardArray[i].title, cardArray[i].body, cardArray[i].id);
     inputFromStorage(cardArray[i]);
-  // cardContainer.innerHTML += `
-  //   <div class="card">
-  //     <header>
-  //       <img src="images/star.svg" alt="Star" class="star" id=${cardObject.id}>
-  //       <img src="images/delete.svg" alt="Delete Icon" class="delete">
-  //     </header>
-  //     <h4>${cardObject[i].title}</h4>
-  //     <p>${cardObject[i].body}</p>
-  //     <footer>
-  //       <img src="images/comment.svg" alt="Comment Icon" class="comment-icon">
-  //       <h5>Comment</h5>
-  //     </footer>
-  //   </div>`;
-  // }
+  }
 }
-}
-// make into method
+
 function favoriteCard(event) {
   var targetId = parseInt(event.target.id, 10);
   for (var i = 0; i < cards.length; i++) {
     if (cards[i].id == event.target.id) {
       cards[i].updateIdea(cards[i]);
       }
-
-    // if (cards[i].favorite) {
-    //   event.target.setAttribute("src","images/star-active.svg");
-    // } else {
-    //   event.target.setAttribute("src","images/star.svg");
-    //   }
-
-
     cards[i].saveToStorage(cards);
-    console.log(localStorage);
-    console.log(cards[i].favorite);
   }
-  };
-// make into method
-// function deleteCard(event) {
-//   for (var i = 0; i < cards.length; i++) {
-//
-//         cards.splice(i, 1);
-//         event.target.closest(".card").remove();
-//         console.log(cards);
-//         // newIdea.deleteFromStorage();
-//   }
-// };
+};
